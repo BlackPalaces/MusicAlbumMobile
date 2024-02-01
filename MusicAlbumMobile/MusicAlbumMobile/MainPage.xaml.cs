999c714b93eb1275b8403e513587324084bd02b5
BlackPalaces <vaname25@gmail.com>
2024-02-01 15:18:53 +0700

    สร้าง drawer ไว้

--- MusicAlbumMobile/MusicAlbumMobile/MainPage.xaml.cs
+++ MusicAlbumMobile/MusicAlbumMobile/MainPage.xaml.cs
@@ -5,14 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
-
+using MusicAlbumMobile.ViewModels;
 namespace MusicAlbumMobile
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : MasterDetailPage
     {
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new MainViewModel();
         }
     }
 }
---
+++ MusicAlbumMobile/MusicAlbumMobile/ViewModels/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MusicAlbumMobile.ViewModels
+{
+     class MainViewModel : INotifyPropertyChanged
+    {
+        public ICommand NavigateCommand { get; }
+
+        public MainViewModel()
+        {
+            NavigateCommand = new Command<string>(Navigate);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private async void Navigate(string page)
+        {
+            // Handle navigation based on the selected menu item
+            switch (page)
+            {
+                case "Item1":
+                    // Navigate to Item 1
+                    break;
+                case "Item2":
+                    // Navigate to Item 2
+                    break;
+                    // Add more cases for additional menu items as needed
+            }
+        }
+
+        // Implement INotifyPropertyChanged interface as needed
+    }
+
+}
